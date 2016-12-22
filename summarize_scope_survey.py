8addc1ec8869cfadedf9b0882d13fc3998746e4f
Oliver Steele <steele@osteele.com>
2016-12-22 11:39:13 -0500

    summarize scope survey: python → pandas

--- summarize_scope_survey.py
+++ summarize_scope_survey.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
 
 # Author: Oliver Steele
-# Date: 2016-12-19
 # License: MIT
 
 import argparse
 import math
 import sys
+from collections import defaultdict
 
 try:
-    from jinja2 import Template
+    from jinja2 import Environment
     import pandas as pd
 except ImportError as e:
     sys.stderr.write('%s. Try running pip install %s' % (e, e.name))
     sys.exit(1)
-
-OVERALL_S = '(overall)'
-OVERALL_KEY = (OVERALL_S,)
 
 parser = argparse.ArgumentParser(description='Create a spreadsheet that summarizes SCOPE P&S results in matrix form.')
 parser.add_argument('-o', '--output', default='SCOPE peer and self reviews.html')
@@ -24,56 +21,25 @@ parser.add_argument('CSV_FILE')
 test_args = (['inputs/SCOPE PandS 12.14.16 - STEELE.csv'],) if 'ipykernel' in sys.modules else ()
 args = parser.parse_args(*test_args)
 
-def resp_fac_to_first_last_name(fullname):
-    return ' '.join(reversed(fullname.split(', ', 2)))
+def create_unique_names_map(entities, short_key_fn=lambda seq:seq[0], long_key_fn=lambda seq:' '.join(seq)):
+    short_names = list(map(short_key_fn, entities))
+    return {entity: short_key if short_names.count(short_key) == 1 else long_key_fn(entity)
+            for entity, short_key in ((entity, short_key_fn(entity)) for entity in entities)}
 
-def row_to_first_last_name(row):
-    return ' '.join([row.part_fname, row.part_lname])
+df = pd.DataFrame.from_csv(args.CSV_FILE, encoding="ISO-8859-1")
 
-def resp_fac_to_name_tuple(surname_comma_firstname):
-    return tuple(reversed(surname_comma_firstname.split(', ', 2)))
+participant_tuples = set(zip(df['part_fname'], df['part_lname']))
+participant_name_map = create_unique_names_map(participant_tuples)
+df['part_name'] = df.apply(lambda row: (row.part_fname, row.part_lname), axis=1).map(participant_name_map)
 
-def unique_name_among(name_tuple, name_tuples):
-    first_name, *_ = name_tuple
-    short_names = [first_name for first_name, *_ in name_tuples]
-    return first_name if short_names.count(first_name) == 1 else ' '.join(*name_tuple)
+part_name_dict = {row.part_uname: row.part_name for row in df[['part_uname', 'part_name']].itertuples()}
+df['eval_name'] = df['eval_uname'].map(part_name_dict)
 
-def unique_names_for(name_tuples):
-    short_names = [first_name for first_name, *_ in name_tuples]
-    return [first_name if short_names.count(first_name) == 1 else ' '.join(first_name, *last_names)
-            for first_name, *last_names in name_tuples]
+first_response_ix = 1 + list(df.columns).index('part_id')
+df2 = df.drop(set(df.columns[:first_response_ix]) - set(['part_name']), axis=1)
 
-def isnan(v):
-    return isinstance(v, float) and math.isnan(v)
-
-def survey_question_matrix(df, column_name):
-    ratings_t = [((row.part_fname, row.part_lname), resp_fac_to_name_tuple(row.resp_fac), getattr(row, column_name))
-                 for row in df.itertuples()]
-    ratings_d = {(rater, ratee): rating for rater, ratee, rating in ratings_t}
-    raters = sorted(set(participant for participant, _, _ in  ratings_t))
-    ratees = sorted(set(participant for _, participant, _ in ratings_t))
-    assert raters == [participant for participant in ratees if participant != OVERALL_KEY], '%s != %s' % (raters, ratees)
-    if set(participant for _, participant, v in ratings_t if not isnan(v)) == set([OVERALL_KEY]):
-        return {participant: ratings_d[participant, OVERALL_KEY] for participant in raters}, True
-    else:
-        return {k: None if isnan(v) else v for k, v in ratings_d.items()}, False
-
-def get_participant_responses(df, participant):
-    responses = [(survey_question, *survey_question_matrix(df, column_name))
-                 for column_name, survey_question in response_columns]
-    overall_responses = [(survey_question, response_matrix[participant])
-                         for survey_question, response_matrix, is_overall_response in responses
-                         if is_overall_response]
-    peer_responses = [(survey_question, response_matrix)
-                      for survey_question, response_matrix, is_overall_response in responses
-                      if not is_overall_response]
-    return overall_responses, peer_responses
-
-survey_df = pd.DataFrame.from_csv(args.CSV_FILE, encoding="ISO-8859-1")
-first_response_ix = 1 + list(survey_df.columns).index('part_id')
-response_columns = [('_%d' % (1 + i), title) for i, title in enumerate(survey_df.columns) if i >= first_response_ix]
-participants = sorted(list(set((row.part_fname, row.part_lname) for row in survey_df.itertuples())))
-participant_responses = [(participant, *get_participant_responses(survey_df, participant)) for participant in participants]
+overall_responses = df2.loc[df['resp_fac'] == '(overall)'].set_index('part_name').dropna(axis=1).select_dtypes(exclude=[int])
+peer_responses = df2.loc[df['resp_fac'] != '(overall)'].set_index(['part_name', 'eval_name']).dropna(axis=1)
 
 HTML_HEADER = """
 <meta charset="UTF-8">
@@ -90,31 +56,35 @@ HTML_HEADER = """
 """
 
 PARTICIPANT_TEMPLATE_TEXT = """
-<section class="participant"><h1>{{ participant }}</h1>
-<dl>
-{% for q in overall_responses %}
-<dt>{{ q[0] }}</dt><dd>{{ q[1] }}</dd>
-{% endfor %}
-</dl>
-{% for q in peer_responses %}
-<section class="survey-question"><div class="survey-question">{{ q[0] }}</div>{{ q[1]|safe }}</section>
-{% endfor %}
+<section class="participant"><h1>{{ participant_name }}</h1>
+    <dl>
+        {% for q, a in overall_responses %}
+            <dt>{{ q }}</dt>
+            <dd>{{ a }}</dd>
+        {% endfor %}
+    </dl>
+    {% for q, a in peer_responses %}
+        <section class="survey-question"><div class="survey-question">{{ q }}</div>{{ a|dataframe }}</section>
+    {% endfor %}
 </section>
 """
-ParticipantTemplate = Template(PARTICIPANT_TEMPLATE_TEXT)
 
-pd.set_option('display.max_colwidth', -1)  # don't truncate cells
+env = Environment()
+env.filters['dataframe'] = lambda df, **kwargs:df.to_html(**kwargs)
+ParticipantTemplate = env.from_string(PARTICIPANT_TEMPLATE_TEXT)
 
-def make_df_html(response_matrix):
-    data = [[response_matrix[participant, p], response_matrix[p, participant]] for p in participants]
-    return pd.DataFrame(data, columns=["Rated others", "Rated by others"], index=unique_names_for(participants))
+def make_peer_df(part_name, col_name):
+    p1 = peer_responses.xs(part_name, level='part_name')
+    p2 = peer_responses.xs(part_name, level='eval_name')
+    df = pd.DataFrame([p1[col_name].rename('Rated'), p2[col_name].rename('Rated by')]).T.replace(0, '')
+    df.index.name = 'other'
+    return df
 
-with open(args.output, 'w') as html_report_f:
-    print(HTML_HEADER, file=html_report_f)
-    for participant, overall_responses, peer_responses in participant_responses:
-        peer_response_htmls = [(survey_question, make_df_html(response_matrix).to_html())
-                               for survey_question, response_matrix in peer_responses]
-        print(ParticipantTemplate.render(participant=unique_name_among(participant, participants),
-                                         overall_responses=overall_responses,
-                                         peer_responses=peer_response_htmls),
-              file=html_report_f)
+with open(args.output, 'w') as report_file:
+    report_file.write(HTML_HEADER)
+    for part_name in sorted(participant_name_map.values()):
+        peer_response_records = [(survey_question, make_peer_df(part_name, survey_question))
+                                 for survey_question in peer_responses.columns]
+        report_file.write(ParticipantTemplate.render(participant_name=part_name,
+                                                     overall_responses=overall_responses.loc[part_name].iteritems(),
+                                                     peer_responses=peer_response_records))
