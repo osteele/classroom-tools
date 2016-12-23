2f75c983781bd42e66b6bcf572ab451161ed4a70
Oliver Steele <steele@osteele.com>
2016-12-23 18:50:37 -0500

    course enrollment flashcards: minor improvements

--- create_course_enrollment_flashcards.py
+++ create_course_enrollment_flashcards.py
@@ -27,8 +27,12 @@ parser.add_argument('--delete', action='store_true')
 parser.add_argument('--service', choices=['dropbox'])
 parser.add_argument('--nicknames', help="Text file list of First “Nick” Last")
 parser.add_argument('HTML_FILE')
-test_args = ['downloads/ENGR2510-1.html', '--nicknames', 'inputs/student-nicknames.txt', '--service', 'dropbox']
-args = parser.parse_args(*((test_args,) if 'ipykernel' in sys.modules else ()))
+
+if 'ipykernel' in sys.modules:
+    sys.argv = ['script', 'downloads/ENGR2510-1.html', '--nicknames', 'config/student-nicknames.txt', '--service', 'dropbox']
+    sys.argv = ['script', 'downloads/ENGR2510-1.html', '-o', 'test.csv']
+
+args = parser.parse_args(sys.argv[1:])
 
 RESIZE_IMAGES = None
 
@@ -54,36 +58,46 @@ def student_fullname(student):
     return ' '.join([get_nickname(student.first_name, student.last_name), student.last_name])
 
 def parse_student_img(elt):
-    last_name, first_name = [s.replace('_', '-') for s in elt.text.strip().split(', ')]
+    last_name, first_name = [s.replace('_', '-') for s in elt.text.strip().split(', ', 2)]
     return Student(first_name.split(' ')[0], last_name, elt.attrs['src'])
 
 html_content = BeautifulSoup(open(args.HTML_FILE), 'html.parser')
+html_content
 
-course_term_field, _, course_number_field, course_name_field = [
-    s.strip() for s in html_content.select('#pg0_V_ggClassList thead tr')[0].text.split('|')]
+course_term_field, _, course_number_field, course_name_field = \
+    [s.strip() for s in html_content.select('#pg0_V_ggClassList thead tr')[0].text.split('|')]
 course_season, course_year = re.search('(Spring|Fall) Term - (\d{4})', course_term_field).groups()
 course_number, course_section = re.match(r'(.+)-(\d+)', course_number_field).groups()
 
 output_basename = '{} {} {} {}'.format(course_number, course_section, course_season, course_year)
-output_path = args.output or os.path.join(args.output_dir or os.path.split(args.HTML_FILE)[0], (args.course_name or output_basename) + '.xlsx')
-media_output_dir = os.path.splitext(output_path)[0]
+output_path = args.output or os.path.join(args.output_dir or os.path.split(args.HTML_FILE)[0], (args.course_name or output_basename)) # + '.xlsx')
+# Default to Excel. Excel is more robust than CSV against unicode.
+if not os.path.splitext(output_path)[1]: output_path += '.xlsx'
 
 students = sorted([parse_student_img(elt) for elt in html_content.select('#pg0_V_ggClassList tbody td img')])
 student_output_images = {student: student_fullname(student) + os.path.splitext(student.img_path)[1]
                          for student in students}
 
 df = pd.DataFrame([['', student_fullname(s), student_output_images[s]] for s in students],
-                 columns=['Text 1', 'Text 2', 'Picture 1'])
+                  columns=['Text 1', 'Text 2', 'Picture 1'])
+df
 
-# Excel is more robust than CSV against unicode
-writer = pd.ExcelWriter(output_path, engine='xlsxwriter')
-df.to_excel(writer, index=False)
-writer.save()
-print('Created', output_path)
-
-os.makedirs(media_output_dir, exist_ok=True)
+extn = os.path.splitext(output_path)[1]
+if extn == '.xlsx':
+    writer = pd.ExcelWriter(output_path, engine='xlsxwriter')
+    df.to_excel(writer, index=False)
+    writer.save()
+elif extn == '.csv':
+    df.to_csv(output_path)
+elif extn:
+    raise Exception("Unknown file extension: %s" % extn[1:])
+else:
+    raise Exception("Missing file extension: %s" % output_path)
+print('Wrote', output_path)
 
 # copy media files
+media_output_dir = os.path.splitext(output_path)[0]
+os.makedirs(media_output_dir, exist_ok=True)
 for student in students:
     srcfile = os.path.join(os.path.join(os.path.split(args.HTML_FILE)[0]), student.img_path)
     dstfile = os.path.join(media_output_dir, student_output_images[student])
@@ -102,7 +116,7 @@ for student in students:
 
 # delete stale media files
 if args.delete:
-    print('remove dead files')
+    print('remove dead files:')
     image_files = set(f for f in os.listdir(media_output_dir) if os.path.isfile(os.path.join(media_output_dir, f)))
     dead_files = image_files - set(student_output_images.values())
     for dead_file in dead_files:
