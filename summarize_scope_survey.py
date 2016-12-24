7753a2c52ee16dbb8ee8ec900df3f75217867a5c
Oliver Steele <steele@osteele.com>
2016-12-24 08:54:48 -0500

    scope survey: separate self-review from table

--- summarize_scope_survey.py
+++ summarize_scope_survey.py
@@ -31,17 +31,21 @@ df['part_name'] = df.apply(lambda row: (row.part_fname, row.part_lname), axis=1)
 
 part_name_dict = {row.part_uname: row.part_name for row in df[['part_uname', 'part_name']].itertuples()}
 df['eval_name'] = df['eval_uname'].map(part_name_dict)
+df['self_eval'] = df['part_name'] == df['eval_name']
 df
 
 first_response_ix = 1 + list(df.columns).index('part_id')
 responses_df = df.drop(df.columns[:first_response_ix], axis=1)
-responses_df['has_peer'] = responses_df.apply(lambda row: pd.notnull(row.eval_name), axis=1)
-responses_df.set_index(['has_peer', 'part_name', 'eval_name'], inplace=True)
+responses_df['has_peer'] = pd.notnull(responses_df['eval_name'])
+responses_df.set_index(['has_peer', 'self_eval', 'part_name', 'eval_name'], inplace=True)
+responses_df
 
-overall_responses = responses_df.loc[False].reset_index(level=1, drop=True).dropna(axis=1).select_dtypes(exclude=[int])
+overall_responses = responses_df.loc[False].reset_index(level=[0,2], drop=True).dropna(axis=1).select_dtypes(exclude=[int])
 overall_responses
 
-peer_responses = responses_df.loc[True].dropna(axis=1)
+self_responses = responses_df.loc[True].loc[True].reset_index(level=0, drop=True).dropna(axis=1)
+
+peer_responses = responses_df.loc[True].loc[False].dropna(axis=1)
 peer_responses
 
 rated_other = peer_responses.copy()
@@ -55,8 +59,6 @@ rated_by = rated_by.reorder_levels([-1, -2], axis=0)
 rated_by.columns = [['This person rated by teammates'] * len(rated_by.columns), rated_by.columns]
 rated_by
 
-# self_reviews = rated_by.loc[pd.Series([a==b for a, b in rated_by.index], rated_by.index)]
-
 nested_peer_responses = pd.concat([rated_by, rated_other], axis=1)
 nested_peer_responses.columns = nested_peer_responses.columns.swaplevel(0, 1)
 nested_peer_responses.sortlevel(0, axis=1, inplace=True)
@@ -67,12 +69,12 @@ HTML_HEADER = """
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
 <style>
     body { margin: 5pt; }
-    section.survey-question { font-style: italic; page-break-inside: avoid; }
     section.participant::after { page-break-after: always; }
-    div.survey-question { font-size: 120% }
-    dt { font-style: italic; margin-top: 10pt; }
-    table { margin-left: 25pt; }
-    th, td { vertical-align: top; }
+    dt { margin-top: 10pt; font-weight: bold; }
+    th { font-weight: normal; font-style: italic; }
+    th, td { vertical-align: top; padding: 2pt; }
+    div.rated-self { margin-top: 5pt; }
+    span.rated-self-label { padding-right: 5pt; font-style: italic; }
 </style>
 """
 
@@ -83,10 +85,14 @@ PARTICIPANT_TEMPLATE_TEXT = """
             <dt>{{ q }}</dt>
             <dd>{{ a }}</dd>
         {% endfor %}
+        {% for q, a, s in peer_responses %}
+            <dt>{{ q }}</dt>
+            <dd>
+                {{ a|dataframe(max_cols=2) }}
+                <div class="rated-self"><span class="rated-self-label">Self:</span> {{ s }}</div>
+            </dd>
+        {% endfor %}
     </dl>
-    {% for q, a in peer_responses %}
-        <section class="survey-question"><div class="survey-question">{{ q }}</div>{{ a|dataframe(max_cols=2) }}</section>
-    {% endfor %}
 </section>
 """
 
@@ -97,7 +103,9 @@ ParticipantTemplate = env.from_string(PARTICIPANT_TEMPLATE_TEXT)
 with open(args.output, 'w') as report_file:
     report_file.write(HTML_HEADER)
     for part_name in sorted(participant_name_map.values()):
-        peer_response_records = [(survey_question, nested_peer_responses.loc[part_name][survey_question])
+        peer_response_records = [(survey_question,
+                                  nested_peer_responses.loc[part_name][survey_question],
+                                  self_responses.loc[part_name][survey_question])
                                  for survey_question in peer_responses.columns]
         report_file.write(ParticipantTemplate.render(participant_name=part_name,
                                                      overall_responses=overall_responses.loc[part_name].iteritems(),
